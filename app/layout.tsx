953df09168c4972bb8d49f86dd104c7ddf936cd5
YounesAEO <y.abouelomoum09@gmail.com>
2025-04-16 00:26:12 +0200

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,27 @@ const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'] });
 export const metadata: Metadata = {
 	title: 'Younes AEO',
 	description: 'Just another random guy on the internet.',
+	icons: {
+		icon: [
+			{ url: '/favicon.ico' },
+			{ url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+			{ url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+		],
+		apple: [{ url: '/apple-touch-icon.png' }],
+		other: [
+			{
+				url: '/android-chrome-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+			},
+			{
+				url: '/android-chrome-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+			},
+		],
+	},
+	manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
